bcde3f79992bf6f9ee7b46bd7a19b0f2c2c3d2ba
Shahin Hojabrian <shahin70000@gmail.com>
2012-09-24 20:38:36 +0300

    sponsors updated

--- parts/sponsors2012.php
+++ parts/sponsors2012.php
@@ -1,105 +1,94 @@
 <div style="width:730px;">
 
-  
-  
-  <table width="730" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td><div class="headergray">PLATINUM SPONSORS</div></td>
-  </tr>
-  
-  <tr>
-    <td><table  style="margin-top:5px;" class="sponsors2012">
+
+<table  style="margin-top:5px;" class="sponsors2012">
     
-    <tr style="height:15px;">
-<td style="text-align:left; width:240px;padding:5px;"><span style="padding-top:5px;color:#636362; font-size:1.5em;">PLATINUM SPONSORS</span>
+
+<tr><td height="25" colspan="3" style="border:thin solid #000; padding-left:25px">
+<!--sponsors row-01-->
+<table>
+<tr style="height:20px;"><td></td></tr>
+<tr>
+<td style="text-align:left; padding:5px; width:240px"><span style="padding-top:5px;color:#636362;">PLATINUM SPONSORS</span>
 </td>
 
-
-
-<td style="text-align:left; width:270px;padding:5px; border-right:thin solid #000">&nbsp;</td>
+<td style="text-align:left;padding:5px; border-left:thin solid #636362; width:160px"><span style="padding-top:5px;color:#636362;">GOLD SPONNSORS</span>
+</td>
+<td style="text-align:left; padding-left:5px; vertical-align:bottom; border-left:thin solid #636362;">
+<span style="padding-top:5px;color:#636362;">PR PARTNERS</span>
+</td>
 
 </tr>
-    <tr>
 
-<td style="text-align:left; width:240px;padding-left:5px; vertical-align:bottom">
+  <td style="text-align:left; padding-left:5px; vertical-align:bottom">
 
-    <a href="http://www.burgan.com" target="_blank"><img width="243px" height="120px" src="img/c2012/sponsorskuwait/burgan.jpg" /></a>                   
-                    
+    <a href="http://www.burgan.com" target="_blank"><img width="121px" height="60px" src="img/c2012/sponsorskuwait/burgan.jpg" /></a>
+    
+<a href="http://www.viva.com.kw/" target="_blank"><img width="104px" height="60px" src="img/c2012/sponsorskuwait/viva.jpg" /></a>
+    
+</td>
+
+<td style="text-align:left;  padding-right:5px; border-left:thin solid #636362;">
+
+
+
+<a href="http://www.un.com.kw/" target="_blank"><img width="100px" height="60px" src="img/c2012/sponsorskuwait/united-network.jpg" /></a>
 
 
 </td>
+<td style="text-align:left; padding-left:5px; vertical-align:bottom; border-left:thin solid #636362;">
+<a href="http://www.way-com.com" target="_blank"><img width="100px" height="60px" src="img/c2012/sponsorskuwait/waycom.jpg" /></a>
+</td>
+</tr>
+<tr style="height:20px;"><td></td></tr>
+</table>
+<!--sponsors row-01 end-->
+
+<!--sponsors row-02-->
+<table><tr>
+<td style="text-align:left; padding:5px; width:120px"><span style="padding-top:5px;color:#636362;">MEDIA PARTNERS</span>
+</td>
+
+<td style="text-align:left;padding:5px; border-left:thin solid #636362;"><span style="padding-top:5px;color:#636362;">SUPPORTERS</span>
+</td>
+<td style="text-align:left; padding-left:5px; vertical-align:bottom; border-left:thin solid #636362; width:120px">
+<span style="padding-top:5px;color:#636362;">OFFICIAL TRANSPORTER</span>
+</td>
+
+</tr>
+
+<td style="text-align:left; padding-left:5px; vertical-align:bottom">
+
+    <a href="http://www.khaleejesque.com/" target="_blank"><img width="104px" height="60px" src="img/c2012/sponsorskuwait/khaleej.jpg" /></a>
+</td>
+
+<td style="text-align:left;  padding-right:5px; border-left:thin solid #636362;">
 
 
+    <a href="http://www.mcdonaldsarabia.com/kuwait/en/home.html" target="_blank"><img width="62px" height="60px" src="img/c2012/sponsorskuwait/macdonalds.jpg" /></a>
 
 
-<td style="text-align:left;  width:270px;padding-right:5px;  border-right:thin solid #000">
+<a href="https://www.qualitynet.org/" target="_blank"><img width="62px" height="60px" src="img/c2012/sponsorskuwait/qualitynet.jpg" /></a>
 
-
+<a href="http://www.dahan.com.kw/" target="_blank"><img width="62px" height="60px" src="img/c2012/sponsorskuwait/dahan.jpg" /></a>
     
  
+<a href="http://www.archofkuwait.com/" target="_blank"><img width="47px" height="60px" src="img/c2012/sponsorskuwait/archofkuwait.jpg" /></a>
 
+<a href="http://www.brownbook.me/" target="_blank"><img width="100px" height="60px" src="img/c2012/sponsorskuwait/brownbook.jpg" /></a>
+
+<a href="http://kuwait-concierge.com/" target="_blank"><img width="62px" height="60px" src="img/c2012/sponsorskuwait/kuwait-concierge.jpg" /></a>
 </td>
-
-
-
-
-
+<td style="text-align:left; padding-left:5px; vertical-align:bottom; border-left:thin solid #636362;">
+<img width="100px" height="60px" src="img/c2012/sponsorskuwait/londonlimo.jpg" />
+</td>
 </tr>
-
-<tr><td height="25" colspan="3"></td></tr>
-
-<tr style="height:15px;">
-<td style="text-align:left; width:240px;padding:5px;"><span style="padding-top:5px;color:#636362;font-size:1.5em;">MEDIA PARTNERS</span>
-</td>
-
-
-
-<td style="text-align:left; width:270px;padding:5px; border-right:thin solid #000; border-left:thin solid #000;"><span style="padding-top:5px;color:#636362;font-size:1.5em; ">SUPPORTERS</span>
-</td>
-
-</tr>
-
-<td style="text-align:left; width:240px;padding-left:5px; vertical-align:bottom">
-
-    <a href="http://www.khaleejesque.com/" target="_blank"><img width="208px" height="120px" src="img/c2012/sponsorskuwait/khaleej.jpg" /></a>                   
-
-
-
-
-
-
-
-</td>
-
-
-
-
-<td style="text-align:left;  width:270px;padding-right:5px; border-left:thin solid #000; border-right:thin solid #000">
-
-
-    <a href="http://www.mcdonaldsarabia.com/kuwait/en/home.html" target="_blank"><img width="124px" height="120px" src="img/c2012/sponsorskuwait/macdonalds.jpg" /></a>
-
-
-<a href="http://www.dahan.com.kw/" target="_blank"><img width="124px" height="120px" src="img/c2012/sponsorskuwait/dahan.jpg" /></a>
-    
- 
-<a href="http://www.archofkuwait.com/" target="_blank"><img width="94px" height="120px" src="img/c2012/sponsorskuwait/archofkuwait.jpg" /></a>
-
-</td>
-
-
-
-
-
-</tr>
-
-<tr><td height="25" colspan="3" style="border-right:thin solid #000"></td></tr>
-
-
+<tr style="height:20px;"><td></td></tr>
+</table>
+</td></tr>
+<!--sponsors row-02 end-->
 </table>
 
-</td>
-  </tr>
-</table>
+
 
 </div>
