166784b3e41c4759819ac40e6b5fb09a82dea21c
shahin70000 <shahin70000@gmail.com>
2012-09-27 10:24:36 +0300

    Two new speakers added
    -Reza Abedini
    -Zahra Alibaba
    
    Email:
    from:  Hussa Al Humaidhi hussa@nuqat.me
    to:  Charchoob Info <info.charchoob@gmail.com>
    cc:  Wakim Zeidan <wakim@nuqat.me>
    date:  Wed, Sep 26, 2012 at 8:32 PM
    subject:  New Speakers Information

---
+++ parts/c2012speakersdetkuwait/Reza-Abedini.php
@@ -0,0 +1,56 @@
+
+        	<table width="360" border="0" align="center" cellpadding="0" cellspacing="0">
+			  <tr>
+			    <td width="135" style="vertical-align:top;"><img src="img/c2012/speakers/Reza-Abedini.jpg" />
+			      <table width="135" border="0" cellspacing="0" cellpadding="0">
+			        <tr>
+			          <td><p><strong>REZA ABEDINI</strong></p></td>
+		            </tr>
+			        <tr>
+			          <td><p style="color:#636362;">Artist/Graphic Designer</p></td>
+		            </tr>
+	            </table></td>
+			    <td width="215" style="vertical-align:top;padding-left:10;">
+                <table width="215" border="0" align="right" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>  <h3>BIO</h3></td>
+  </tr>
+  <tr>
+    <td>Reza Abedini got his diploma in visual arts from The School of Fine Arts in Tehran (1985) and then continued his studies in painting and fine arts and in 1989 graduated from Art University of Tehran. From then on he has been working as a designer, teacher and researcher in the field of Typography, Graphic Design and visual arts in general.
+
+Among the books that have been published on his works are: 
+In the Beginning, 2003 
+Design and Designer (Reza Abedini), 2004
+Vision of Design (Reza Abedini), 2007
+Margin, 2008 
+
+Abedini’s professional focus of attention is Persian and Arabic script and Typography and has had numerous lectures and articles on the subject. One of his recent activities in these fields is launching a research group called Dabireh which has published its first issue of quarterly in summer 2010 with the same title ( Dabireh ) covering subjects of script, language, writing and designing Persian- Arabic types and letters.
+<br/><br/>
+<a class="nav-twotwelve" target="_blank" href="http://www.rezaabedini.com/">rezaabedini.com</a><br /></td>
+  </tr>
+</table>
+
+              
+                </td>
+			  </tr>
+			  <tr>
+			    <td style="padding-top:10px;padding-bottom:10px;">&nbsp;</td>
+			    <td>&nbsp;</td>
+			  </tr>
+			  <tr>
+			    <td>&nbsp;</td>
+			    <td>
+                <table width="225" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><h3>LECTURE TITLE:</h3></td>
+  </tr>
+  <tr>
+    <td><span style="color:#E94F1B;line-height:12pt;font-size:12pt;">TBD</span></td>
+  </tr>
+</table>
+
+               
+                </td>
+			  </tr>
+			</table>
+            <!-- google ana-->  
---
+++ parts/c2012speakersdetkuwait/Zahra-Alibaba.php
@@ -0,0 +1,46 @@
+
+        	<table width="360" border="0" align="center" cellpadding="0" cellspacing="0">
+			  <tr>
+			    <td width="135" style="vertical-align:top;"><img src="img/c2012/speakers/Zahra-Alibaba.jpg" />
+			      <table width="135" border="0" cellspacing="0" cellpadding="0">
+			        <tr>
+			          <td><p><strong>ZAHRA ALIBABA</strong></p></td>
+		            </tr>
+			        <tr>
+			          <td><p style="color:#636362;">Architect/Curator</p></td>
+		            </tr>
+	            </table></td>
+			    <td width="215" style="vertical-align:top;padding-left:10;">
+                <table width="215" border="0" align="right" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>  <h3>BIO</h3></td>
+  </tr>
+  <tr>
+    <td>BArch Kuwait University 2008, works with Kuwait National Council of Culture, Arts and Letters ( NCCAL ), involved in restoration and rehabilitation projects. Co-Curator of Winter School Middle East, and Curator of Kuwait Pavilion at Venice Biennale 2012.</td>
+  </tr>
+</table>
+
+              
+                </td>
+			  </tr>
+			  <tr>
+			    <td style="padding-top:10px;padding-bottom:10px;">&nbsp;</td>
+			    <td>&nbsp;</td>
+			  </tr>
+			  <tr>
+			    <td>&nbsp;</td>
+			    <td>
+                <table width="225" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><h3>LECTURE TITLE:</h3></td>
+  </tr>
+  <tr>
+    <td><span style="color:#E94F1B;line-height:12pt;font-size:12pt;">Form of Question</span></td>
+  </tr>
+</table>
+
+               
+                </td>
+			  </tr>
+			</table>
+            <!-- google ana-->  
